f89cb4607048b6aff05f06f4cc12b4aa92411dc4
ghiggie <ghiggie@uab.edu>
2019-10-03 17:51:30 -0500

    Removed some intermediate calculations in the bath correlation functions. Trying to make the code more efficient.

--- QNB1_New/bath.f95
+++ QNB1_New/bath.f95
@@ -8,26 +8,27 @@ module bath
 
  contains
 
+    ! This function computes the bath correlation function. Right now, it only
+    ! uses a single Matsubara exponent.
     function bc(temp, gamma, lambda, t1, t2)
         real(kind=DP), intent(in) :: temp, gamma, lambda, t1, t2
         complex(kind=DP) :: bc
         real(kind=DP) :: cotan, denom, tau
-        complex(kind=DP) :: c1, c2, c3, tmp
+        complex(kind=DP) :: c1, c2, c3
 
         tau = t1 - t2 ! Note that t1 > t2
         cotan = cos(0.5*gamma/temp)/sin(0.5*gamma/temp)
         denom = (2*PI)**2 - (gamma/temp)**2
 
-        c1 = 2*lambda*gamma*(REAL1*cotan + (-1)*IMAG1)
+        c1 = 2*lambda*gamma*CMPLX(cotan, -1)
         c2 = 8*PI*lambda*gamma/denom
         c3 = 4*lambda*temp/gamma - 2*lambda*cotan - (8*lambda*gamma/temp)/denom
 
         if (tau .eq. 0) then
-            tmp = c1 + c2 + c3
+            bc = c1 + c2 + c3
         else
-            tmp = c1*exp(-gamma*tau) + c2*exp(-2*PI*temp*tau)
+            bc = c1*exp(-gamma*tau) + c2*exp(-2*PI*temp*tau)
         end if
-        bc = tmp
     end function bc
 
     function lambda_bc(A, B, temp, gamma, lambda, t, dt)
@@ -36,44 +37,39 @@ module bath
         complex(kind=DP), dimension(:,:), allocatable :: lambda_bc
         integer :: n, M, j
         real(kind=DP) :: tj, tc
-        complex(kind=DP), dimension(:,:), allocatable :: tmp, tmp1, tmp2
         complex(kind=DP), dimension(:,:), allocatable :: j1, j2, j3, j4
 
         n = size(A, dim=1)
         allocate(lambda_bc(n,n))
-        allocate(tmp(n,n))
-        allocate(tmp1(n,n))
-        allocate(tmp2(n,n))
         allocate(j1(n,n))
         allocate(j2(n,n))
         allocate(j3(n,n))
         allocate(j4(n,n))
 
-        tmp = 0
+        lambda_bc = 0
         M = int(t / dt)
         do j = 0, M - 1
             tj = j * dt
 
+            ! First RK4 step
             tc = tj
-            tmp1 = (t - tc) * A
-            tmp2 = ExpOp(tmp1, B)
-            j1 = bc(temp, gamma, lambda, t, tc) * tmp2
+            j1 = bc(temp, gamma, lambda, t, tc) * ExpOp((t - tc) * A, B)
 
+            ! Second RK4 Step
             tc = tj + dt / 2
-            tmp1 = (t - tc) * A
-            tmp2 = ExpOp(tmp1, B)
-            j2 = bc(temp, gamma, lambda, t, tc) * tmp2
+            j2 = bc(temp, gamma, lambda, t, tc) * ExpOp((t - tc) * A, B)
 
+            ! Because the integral doesn't itself depend on the output function,
+            ! the third RK4 step is the same as the second RK4 step.
             j3 = j2
 
+            ! Fourth RK4 step
             tc = tj + dt
-            tmp1 = (t - tc) * A
-            tmp2 = ExpOp(tmp1, B)
-            j4 = bc(temp, gamma, lambda, t, tc) * tmp2
+            j4 = bc(temp, gamma, lamba, t, tc) * ExpOp((t - tc) * A, B)
 
-            tmp = tmp + (dt / 6) * (j1 + 2*j2 + 2*j3 + j4)
+            ! Finish the RK4 iteration
+            lambda_bc = lambda_bc + (dt / 6) * (j1 + 2*j2 + 2*j3 + j4)
         end do
-        lambda_bc = tmp
     end function lambda_bc
 
 end module bath
