c2e976c560192e9cc4a0a1260938b83d445071b6
ghiggie <ghiggie@uab.edu>
2019-10-14 12:32:57 -0500

    Changed Integration Scheme for Lambda
    I've changed the integration scheme from RK4 to Simpson.

--- QNB1_New/bath.f95
+++ QNB1_New/bath.f95
@@ -35,43 +35,39 @@ module bath
         complex(kind=DP), dimension(:,:), intent(in) :: A, B
         complex(kind=DP), dimension(:,:), allocatable :: lambda_bc
         integer :: n, M, j
-        real(kind=DP) :: tj, tc
+        real(kind=DP) :: t2j, t2j1, t2j2
         complex(kind=DP), dimension(:,:), allocatable :: tmp, tmp1, tmp2
-        complex(kind=DP), dimension(:,:), allocatable :: j1, j2, j3, j4
+        complex(kind=DP), dimension(:,:), allocatable :: f2j, f2j1, f2j2
 
         n = size(A, dim=1)
         allocate(lambda_bc(n,n))
         allocate(tmp(n,n))
         allocate(tmp1(n,n))
         allocate(tmp2(n,n))
-        allocate(j1(n,n))
-        allocate(j2(n,n))
-        allocate(j3(n,n))
-        allocate(j4(n,n))
+        allocate(f2j(n,n))
+        allocate(f2j1(n,n))
+        allocate(f2j2(n,n))
 
         tmp = 0
         M = int(t / dt)
-        do j = 0, M - 1
-            tj = j * dt
+        do j = 0, M/2 - 1
+            t2j = 2*j * dt
+            t2j1 = t2j + dt
+            t2j2 = t2j1 + dt
 
-            tc = tj
-            tmp1 = (t - tc) * A
+            tmp1 = (t - t2j) * A
             tmp2 = ExpOp(tmp1, B)
-            j1 = bc(temp, gamma, lambda, t, tc) * tmp2
+            f2j = bc(temp, gamma, lambda, t, t2j) * tmp2
 
-            tc = tj + dt / 2
-            tmp1 = (t - tc) * A
+            tmp1 = (t - t2j1) * A
             tmp2 = ExpOp(tmp1, B)
-            j2 = bc(temp, gamma, lambda, t, tc) * tmp2
+            f2j1 = bc(temp, gamma, lambda, t, t2j1) * tmp2
 
-            j3 = j2
-
-            tc = tj + dt
-            tmp1 = (t - tc) * A
+            tmp1 = (t - t2j2) * A
             tmp2 = ExpOp(tmp1, B)
-            j4 = bc(temp, gamma, lambda, t, tc) * tmp2
+            f2j2 = bc(temp, gamma, lambda, t, t2j2) * tmp2
 
-            tmp = tmp + (dt / 6) * (j1 + 2*j2 + 2*j3 + j4)
+            tmp = tmp + (dt / 3) * (f2j + 4*f2j1 + f2j2)
         end do
         lambda_bc = tmp
     end function lambda_bc
